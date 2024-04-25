a9a933cbd2c152266bbbbd633b1bcdb02de4013a
Ilam Ball <informasiilam@gmail.com>
2024-04-25 14:49:03 +0700

    membuat else, while, if, dan return

--- linklist.cpp
+++ linklist.cpp
@@ -113,4 +113,16 @@ void seacrhData() {
         system("cls");
         return;
     }
+    else {
+        int nim;
+        cout << "Masukkan NIM: ";
+        cin >> nim;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            if (currentNode->noMhs == nim) {
+                cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+                return;
+            }
+        }
+    }
 }
